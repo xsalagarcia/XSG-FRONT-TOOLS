afab8dc332a5bbfd02017b694c812a6364f3cce6
xsalagarcia <xsalagarcia@gmail.com>
2024-03-19 15:13:25 +0100

    cislider, index adjusts

--- js/addons/cislider.js
+++ js/addons/cislider.js
@@ -11,15 +11,19 @@ const cisliders = document.querySelectorAll(".cislider");
  * @param {Number} totalItems The number of items.
  */
 function populateNavigation(div, totalItems, cisliderDiv) {
+    cisliderDiv.circles = []
     for (let i = 0; i < totalItems; i++) {
         const circle = document.createElement("span");
         circle.classList.add("cislider-circle");
         div.appendChild(circle);
+        cisliderDiv.circles.push(circle);
         circle.addEventListener("click", () => {
+            cisliderDiv.circles[cisliderDiv.position].classList.remove("active");
             cisliderDiv.position = i;
             setCisliderVisibleImage(cisliderDiv);
-        })
+        });
     }
+    cisliderDiv.circles[0].classList.add("active");
 }
 
 /**
@@ -31,11 +35,13 @@ function setCisliderVisibleImage(cisliderDiv){
         cisliderDiv.nextBtn.style.visibility = (cisliderDiv.position >= cisliderDiv.totalItems -1)? 'hidden' : 'visible';
     if (cisliderDiv.prevBtn)
         cisliderDiv.prevBtn.style.visibility = (cisliderDiv.position < 1)? 'hidden' : 'visible';
+    if (cisliderDiv.circles)
+        cisliderDiv.circles[cisliderDiv.position].classList.add("active");
     
         cisliderDiv.querySelectorAll(".cislider>ul>li").forEach((li) =>{
-        const itemWidth = parseFloat(window.getComputedStyle(li).width);
-        li.style.translate = `${- cisliderDiv.position * itemWidth}px`;
-    });
+            const itemWidth = parseFloat(window.getComputedStyle(li).width);
+            li.style.translate = `${- cisliderDiv.position * itemWidth}px`;
+        });
 }
 
 
@@ -54,6 +60,8 @@ cisliders.forEach((cisliderDiv) => {
         if (cisliderDiv.position >= cisliderDiv.totalItems -1)
             cisliderDiv.nextBtn.style.visibility = 'hidden';
         cisliderDiv.nextBtn.addEventListener('click', ()=>{
+            if (cisliderDiv.circles)
+                cisliderDiv.circles[cisliderDiv.position].classList.remove("active");
             cisliderDiv.position++;
             setCisliderVisibleImage(cisliderDiv);
         });
@@ -61,6 +69,8 @@ cisliders.forEach((cisliderDiv) => {
     if (cisliderDiv.prevBtn) {
         cisliderDiv.prevBtn.style.visibility = 'hidden';
         cisliderDiv.prevBtn.addEventListener('click', ()=>{
+            if (cisliderDiv.circles)
+                cisliderDiv.circles[cisliderDiv.position].classList.remove("active");
             cisliderDiv.position--;
             setCisliderVisibleImage(cisliderDiv);
         });
