29f80e832c10ef6cc228ec32c5e8169c72245043
xsalagarcia <xsalagarcia@gmail.com>
2024-03-19 06:42:55 +0100

    bugs correction on cslider, added cislider

---
+++ js/addons/cislider.js
@@ -0,0 +1,68 @@
+/**
+ * This is linked to cislider.css.
+ */
+
+/**Represents all the cislider */
+const cisliders = document.querySelectorAll(".cislider");
+
+/**
+ * Populates the cislider-navigation with nav cislider-circles.
+ * @param {Element} div The div with cislider-navigation class.
+ * @param {Number} totalItems The number of items.
+ */
+function populateNavigation(div, totalItems, cisliderDiv) {
+    for (let i = 0; i < totalItems; i++) {
+        const circle = document.createElement("span");
+        circle.classList.add("cislider-circle");
+        div.appendChild(circle);
+        circle.addEventListener("click", () => {
+            cisliderDiv.position = i;
+            setCisliderVisibleImage(cisliderDiv);
+        })
+    }
+}
+
+/**
+ * Changes the image of the cislider.
+ * @param {Element} cisliderDiv The cislider.
+ */
+function setCisliderVisibleImage(cisliderDiv){
+    if (cisliderDiv.nextBtn)
+        cisliderDiv.nextBtn.style.visibility = (cisliderDiv.position >= cisliderDiv.totalItems -1)? 'hidden' : 'visible';
+    if (cisliderDiv.prevBtn)
+        cisliderDiv.prevBtn.style.visibility = (cisliderDiv.position < 1)? 'hidden' : 'visible';
+    
+        cisliderDiv.querySelectorAll(".cislider>ul>li").forEach((li) =>{
+        const itemWidth = parseFloat(window.getComputedStyle(li).width);
+        li.style.translate = `${- cisliderDiv.position * itemWidth}px`;
+    });
+}
+
+
+
+cisliders.forEach((cisliderDiv) => {
+    cisliderDiv.position = 0;
+    cisliderDiv.totalItems =  cisliderDiv.querySelectorAll(".cislider>ul>li").length;
+    cisliderDiv.nextBtn = cisliderDiv.querySelector(".cislider>.cislider-next");
+    cisliderDiv.prevBtn = cisliderDiv.querySelector(".cislider>.cislider-prev");
+    const divNavigation = cisliderDiv.querySelector(".cislider>.cislider-navigation");
+    if (divNavigation)
+        cisliderDiv.cisliderCirclers = populateNavigation(divNavigation, cisliderDiv.totalItems, cisliderDiv);
+
+
+    if (cisliderDiv.nextBtn) {
+        if (cisliderDiv.position >= cisliderDiv.totalItems -1)
+            cisliderDiv.nextBtn.style.visibility = 'hidden';
+        cisliderDiv.nextBtn.addEventListener('click', ()=>{
+            cisliderDiv.position++;
+            setCisliderVisibleImage(cisliderDiv);
+        });
+    }
+    if (cisliderDiv.prevBtn) {
+        cisliderDiv.prevBtn.style.visibility = 'hidden';
+        cisliderDiv.prevBtn.addEventListener('click', ()=>{
+            cisliderDiv.position--;
+            setCisliderVisibleImage(cisliderDiv);
+        });
+    }
+});
--- js/addons/cslider.js
+++ js/addons/cslider.js
@@ -5,9 +5,9 @@
 const csliders = document.querySelectorAll(".cslider");
 csliders.forEach((csliderDiv) => {
     csliderDiv.position = 0;
-    csliderDiv.totalItems =  document.querySelectorAll(".cslider>ul").length;
-    csliderDiv.nextBtn = document.querySelector(".cslider>.next");
-    csliderDiv.prevBtn = document.querySelector(".cslider>.prev");
+    csliderDiv.totalItems =  csliderDiv.querySelectorAll(".cslider>ul>li").length;
+    csliderDiv.nextBtn = csliderDiv.querySelector(".cslider>.next");
+    csliderDiv.prevBtn = csliderDiv.querySelector(".cslider>.prev");
     csliderDiv.prevBtn.style.visibility = 'hidden';
     if (csliderDiv.position > csliderDiv.totalItems + 2)
                 csliderDiv.nextBtn.style.visibility = 'hidden';
@@ -15,18 +15,18 @@ csliders.forEach((csliderDiv) => {
     csliderDiv.querySelector(".cslider>.next").addEventListener('click', ()=>{
         csliderDiv.prevBtn.style.visibility = 'visible';
         csliderDiv.position++;
-        document.querySelectorAll(".cslider>ul>li").forEach((li) =>{
+        csliderDiv.querySelectorAll(".cslider>ul>li").forEach((li) =>{
             const itemWidth = parseFloat(window.getComputedStyle(li).width);
             li.style.translate = `${- csliderDiv.position * itemWidth}px`;
         });
-        if (csliderDiv.position > csliderDiv.totalItems + 3)
+        if (csliderDiv.position >= csliderDiv.totalItems -1)
             csliderDiv.nextBtn.style.visibility = 'hidden';
     });
     csliderDiv.querySelector(".cslider>.prev").addEventListener('click', ()=>{
 
             csliderDiv.nextBtn.style.visibility = 'visible';
             csliderDiv.position--;
-            document.querySelectorAll(".cslider>ul>li").forEach((li) =>{
+            csliderDiv.querySelectorAll(".cslider>ul>li").forEach((li) =>{
                 const itemWidth = parseFloat(window.getComputedStyle(li).width);
                 li.style.translate = `${-csliderDiv.position * itemWidth}px`;
             });
