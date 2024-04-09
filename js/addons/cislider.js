47244c485da4f8968d972acebaacca1bc5934a78
xsalagarcia <xsalagarcia@gmail.com>
2024-04-09 12:19:34 +0200

    cislider update. Big modification on cislider.js. A class reducing global namespace names. Options by JS

--- js/addons/cislider.js
+++ js/addons/cislider.js
@@ -1,104 +1,146 @@
+/*This is linked to cislider.css*/
+
 /**
+ * Cislider class represents a cislider. A slider that can have index, prev and next button.
+ * See cislider.css for how to use it.
  * This is linked to cislider.css.
  */
-
-
-
-/**
- * Populates the cislider-navigation with nav cislider-circles.
- * @param {Element} div The div with cislider-navigation class.
- * @param {Number} totalItems The number of items.
- * @param {Element} csliderDiv The cslider-navigation Div.
- */
-function populateNavigation(div, totalItems, cisliderDiv) {
-    cisliderDiv.circles = []
-    for (let i = 0; i < totalItems; i++) {
-        const circle = document.createElement("span");
-        circle.classList.add("cislider-circle");
-        div.appendChild(circle);
-        cisliderDiv.circles.push(circle);
-        circle.addEventListener("click", () => {
-            cisliderDiv.circles[cisliderDiv.position].classList.remove("active");
-            cisliderDiv.position = i;
-            setCisliderVisibleImage(cisliderDiv);
-        });
-    }
-    cisliderDiv.circles[0].classList.add("active");
-}
-
-/**
- * Changes the image of the cislider.
- * @param {Element} cisliderDiv The cislider.
- * @param {Boolean} stopInterval pass false if don't want to stop automatic change. Ideally, when the change is automatic.
- */
-function setCisliderVisibleImage(cisliderDiv, stopInterval=true){
-    if (cisliderDiv.intervalId && stopInterval){ 
-        clearInterval(cisliderDiv.intervalId);
-        cisliderDiv.intervalId = null;
-    }
-
-    if (cisliderDiv.nextBtn)
-        cisliderDiv.nextBtn.style.visibility = (cisliderDiv.position >= cisliderDiv.totalItems -1)? 'hidden' : 'visible';
-    if (cisliderDiv.prevBtn)
-        cisliderDiv.prevBtn.style.visibility = (cisliderDiv.position < 1)? 'hidden' : 'visible';
-    if (cisliderDiv.circles)
-        cisliderDiv.circles[cisliderDiv.position].classList.add("active");
+class Cislider {
     
-        cisliderDiv.querySelectorAll(".cislider>ul>li").forEach((li) =>{
-            const itemWidth = parseFloat(window.getComputedStyle(li).width);
-            li.style.translate = `${- cisliderDiv.position * itemWidth}px`;
+    /**The active slide. */
+    position;
+
+    /**The number of items. */
+    totalItems;
+
+    /**Reference to next button (A span). */
+    nextBtn;
+
+    /**Reference to previous button (A span). */
+    prevBtn;
+
+    /**Reference to the div that contains the index .*/
+    index;
+
+    /**A list that contains the references to each div showed as a circle in the index. */
+    indexCircles = null;
+
+    /**interval id to stop autochange. */
+    intervalId;
+
+    /**A list with ul>li */
+    listItems;
+
+    /**
+     * 
+     * @param {HTMLDivElement} cisliderDiv 
+     */
+    constructor(cisliderDiv){
+        if (!(cisliderDiv instanceof HTMLDivElement) && !cisliderDiv.classList.contains("cislider"))
+            throw new TypeError("HTMLDivElement with cislider class was expected");
+        
+        this.position = 0;
+        this.totalItems = cisliderDiv.querySelectorAll(".cislider>ul>li").length;
+        this.listItems = cisliderDiv.querySelectorAll(".cislider>ul>li");
+
+        const options = cisliderDiv.getAttribute("cisliderOptions").split(" ");
+
+        options.forEach(option => {
+            if (/\binterval(?:\d+)?\b/.test(option)){
+                let timeInterval = parseInt(option.replace(/^\D+/g, ''));
+                if (!timeInterval)
+                    timeInterval = 5000;
+
+                this.intervalId = setInterval(this.autoChange, timeInterval, this)
+            }
+
+            switch(option) {
+                case "prev":
+                    this.prevBtn = document.createElement("span");
+                    this.prevBtn.classList.add("cislider-prev");
+                    cisliderDiv.insertBefore(this.prevBtn, cisliderDiv.firstChild);
+                    this.prevBtn.addEventListener('click', ()=>{
+                        if (this.indexCircles)
+                        this.indexCircles[this.position].classList.remove("active");
+                        this.position--;
+                        this.setCisliderVisibleImage();
+                    });
+                    break;
+                case "next":
+                    this.nextBtn = document.createElement("span");
+                    this.nextBtn.classList.add("cislider-next");
+                    cisliderDiv.appendChild(this.nextBtn);
+                    this.nextBtn.addEventListener('click', ()=>{
+                        if (this.indexCircles)
+                            this.indexCircles[this.position].classList.remove("active");
+                        this.position++;
+                        this.setCisliderVisibleImage();
+                    });
+                    break;
+                case "index": /*Its position is absolute*/
+                    this.index = document.createElement("div");
+                    this.index.classList.add("cislider-navigation");
+                    this.populateNavigation()
+                    cisliderDiv.appendChild(this.index);
+                break;
+            }
         });
-}
+    }
 
-/**
- * Changes to next automatically.
- * @param {Element} csliderDiv The cslider
- */
-function autoChange(cisliderDiv){
-    console.log("autochange");
-    if (cisliderDiv.circles)
-        cisliderDiv.circles[cisliderDiv.position].classList.remove("active");
+    /**
+     * Populates the this.#index. That is, creates this.#indexCircles and appends the nodes to this.#index.
+     */
+    populateNavigation(){
+        this.indexCircles = [];
+        for (let i = 0; i < this.totalItems; i++){
+            const circle = document.createElement("span");
+            circle.classList.add("cislider-circle");
+            this.index.appendChild(circle);
+            this.indexCircles.push(circle);
+            circle.addEventListener("click", (event) =>{
+                this.indexCircles[this.position].classList.revome("active");
+                this.position = i;
+                this.setCisliderVisibleImage();
+            });
+        }
+        this.indexCircles[0].classList.add("active");
+    }
 
-    cisliderDiv.position = (cisliderDiv.position + 1) % cisliderDiv.totalItems;
-    setCisliderVisibleImage(cisliderDiv, false);
+    /**
+     * 
+     * @param {Boolean} stopInterval 
+     */
+    setCisliderVisibleImage(stopInterval=true){
+        if (this.intervalId && stopInterval){ 
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    
+        if (this.nextBtn)
+            this.nextBtn.style.visibility = (this.position >= this.totalItems -1)? 'hidden' : 'visible';
+        if (this.prevBtn)
+            this.prevBtn.style.visibility = (this.position < 1)? 'hidden' : 'visible';
+        if (this.indexCircles)
+            this.indexCircles[this.position].classList.add("active");
+        
+        this.listItems.forEach((li) =>{
+            const itemWidth = parseFloat(window.getComputedStyle(li).width);
+            li.style.translate = `${- this.position * itemWidth}px`;
+        });
+    }
+
+    autoChange(cislider){
+        if (cislider.indexCircles)
+            cislider.indexCircles[cislider.position].classList.remove("active");
+
+        cislider.position = (cislider.position + 1) % cislider.totalItems;
+        cislider.setCisliderVisibleImage(false);
+    }
+
 }
 
 
 
 document.querySelectorAll(".cislider").forEach((cisliderDiv) => {
-    /*added atr for csliderDiv:
-     *position, totalItems, nextBtn, prevBtn, circles, intervalId
-    */
-    cisliderDiv.position = 0;
-    cisliderDiv.totalItems =  cisliderDiv.querySelectorAll(".cislider>ul>li").length;
-    cisliderDiv.nextBtn = cisliderDiv.querySelector(".cislider>.cislider-next");
-    cisliderDiv.prevBtn = cisliderDiv.querySelector(".cislider>.cislider-prev");
-    const divNavigation = cisliderDiv.querySelector(".cislider>.cislider-navigation, .cislider>.cislider-navigation-outer");
-    if (divNavigation)
-        cisliderDiv.cisliderCirclers = populateNavigation(divNavigation, cisliderDiv.totalItems, cisliderDiv);
-
-
-    if (cisliderDiv.nextBtn) {
-        if (cisliderDiv.position >= cisliderDiv.totalItems -1)
-            cisliderDiv.nextBtn.style.visibility = 'hidden';
-        cisliderDiv.nextBtn.addEventListener('click', ()=>{
-            if (cisliderDiv.circles)
-                cisliderDiv.circles[cisliderDiv.position].classList.remove("active");
-            cisliderDiv.position++;
-            setCisliderVisibleImage(cisliderDiv);
-        });
-    }
-    if (cisliderDiv.prevBtn) {
-        cisliderDiv.prevBtn.style.visibility = 'hidden';
-        cisliderDiv.prevBtn.addEventListener('click', ()=>{
-            if (cisliderDiv.circles)
-                cisliderDiv.circles[cisliderDiv.position].classList.remove("active");
-            cisliderDiv.position--;
-            setCisliderVisibleImage(cisliderDiv);
-        });
-    }
-
-    /*intervalId attribute is used to stop autochanges when user click prev, next or index*/
-    if (cisliderDiv.classList.contains("cislider-auto"))
-        cisliderDiv.intervalId = setInterval(autoChange, 5000, cisliderDiv);
+    new Cislider(cisliderDiv);
 });
