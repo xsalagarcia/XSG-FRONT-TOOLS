5d05a50f2300a615089b556c5732856a92b8506b
xsalagarcia <xsalagarcia@gmail.com>
2024-05-29 17:21:16 +0200

    code improved on apperaing-on-scroll.js

--- js/addons/appearing-on-scroll.js
+++ js/addons/appearing-on-scroll.js
@@ -1,14 +1,14 @@
 (function(){
     const percentageRevelation = 0.5; //when the appearing section will change (0.5 is at half window).
+    const appearingSections = document.querySelectorAll(".appearing-section");
     window.addEventListener("scroll", ()=>{
-        let windowHeight = window.innerHeight;
-        document.querySelectorAll(".appearing-section").forEach(element => {
-            let top = element.getBoundingClientRect().top; // From size and position of the rectangle, get the top.
+        const windowHeight = window.innerHeight;
+        appearingSections.forEach(element => {
+            const top = element.getBoundingClientRect().top; // From size and position of the rectangle, get the top.
             if (top < windowHeight - windowHeight * percentageRevelation){
                 element.classList.add("active");
             } else {
                 element.classList.remove("active");
-                console.log("inactive")
             }
         });
     });
